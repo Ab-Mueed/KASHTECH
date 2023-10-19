8b5daa1588ba7c0b99eea8e53512c98a2321261b
Ab-Mueed <95278577+Ab-Mueed@users.noreply.github.com>
2023-10-19 18:37:53 +0530

    Add files via upload

---
+++ 01_JavaScript.js
@@ -0,0 +1,7 @@
+function function1() {
+    document.getElementById("slide").src="";
+    document.getElementById("name").innerHTML='THEO HALL';
+    document.getElementById("description").innerHTML='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,totam rem aperiam, eaque ipsa quae ab.';
+
+  }
+
